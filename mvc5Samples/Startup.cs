cb2265d072359e43c59742ce302b58a6b8854b33
yhhno <516061849@qq.com>
2015-11-13 00:07:03 +0800

    1.first

---
+++ mvc5Samples/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc5Samples.Startup))]
+namespace mvc5Samples
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
