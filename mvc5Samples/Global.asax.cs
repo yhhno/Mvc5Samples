e9d492e36357a65a81baea92abeba86c5217c434
yhhno <516061849@qq.com>
2016-04-19 16:53:49 +0800

    add  miniprofiler

--- mvc5Samples/Global.asax.cs
+++ mvc5Samples/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StackExchange.Profiling;
 
 namespace mvc5Samples
 {
@@ -16,6 +17,22 @@ namespace mvc5Samples
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+
+
+       //添加监控
+        protected void Application_BeginRequest()
+        {
+            if (Request.IsLocal)//这里是允许本地访问启动监控，可不写
+            {
+                MiniProfiler.Start();
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
         }
     }
 }
